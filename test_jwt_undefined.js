aa1d18171bd22953e04fbd28cf62614f08b76de5
laysontrue-prog <laysontrue@gmail.com>
2026-01-11 00:51:46 +0800

    1-11 更新

--- test_jwt_undefined.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-try {
-    const token = jwt.sign({ foo: 'bar' }, undefined);
-    console.log('Signed with undefined secret:', token);
-} catch (e) {
-    console.log('Error signing with undefined secret:', e.message);
-}
